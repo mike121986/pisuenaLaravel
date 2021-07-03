91b1c26f3f7cc097201f228f465c7d01ab7397c3
mike trujillo <38016591+mike121986@users.noreply.github.com>
2021-07-03 14:03:40 -0500

    este es el primer commit de la aplicacion de laravel en rio pisueña

---
+++ database/migrations/2021_07_03_171639_create_sub_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('slug');
+            $table->string('image');
+            $table->unsignedBigInteger('category_id');
+            $table->boolean('color');
+            $table->boolean('size');
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
