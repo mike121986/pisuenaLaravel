506c6f4f55ea17b21df5a9d5b48f2763e366daf7
mike trujillo <38016591+mike121986@users.noreply.github.com>
2021-07-07 14:18:22 -0500

    se remplaza el codigo para insertar el componente navigation-subcategory

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,14 +1,21 @@
 <style>
-    #navigation-menu{
+    #navigation-menu {
         height: calc(100vh - 4rem);
     }
+
+    .navigation-link:hover .navigation-submenu {
+        display: block !important
+    }
+
 </style>
 
-<header class="bg-trueGray-700 sticky top-0">
+<header class="bg-trueGray-700 sticky top-0" x-data="{open:false}">
     <div class="container flex items-center h-16">
-        <a class="flex flex-col items-center px-4 justify-center bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+        <a
+            class="flex flex-col items-center px-4 justify-center bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16" />
             </svg>
 
             <span>Categorías</span>
@@ -47,7 +54,7 @@
                             @csrf
 
                             <x-jet-dropdown-link href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                                            this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-jet-dropdown-link>
                         </form>
@@ -75,15 +82,47 @@
 
     <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
         <div class="container h-full">
-            <div class="grid grid-cols-4 h-full">
+            <div class="grid grid-cols-4 h-full relative">
                 <ul class="bg-white">
- 
+                    @foreach ($categories as $category)
+                        <li class=" navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                            <a href="" class="py-2 px-4 text-sm flex items-center">
+                                <span class="flex justify-center w-9">
+                                    {!! $category->icon !!}
+                                </span>
+                                {{ $category->name }}
+                            </a>
+                            <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+                                {{-- <div class="grid grid-cols-4 p-4">
+                                    <div>
+                                        <p class="text-lg font-bold text-center text-trueGray-500 mb-3">Subcategorias
+                                        </p>
+                                        <ul>
+                                            @foreach ($category->subcategories as $subcategory)
+                                                <li>
+                                                    <a href=""
+                                                        class="text-trueGray-500 inline-block font-semibold py-1 px-4 hover:text-orange-500">
+                                                        {{ $subcategory->name }}
+                                                    </a>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                    <div class="col-span-3">
+                                        <img class="h-64 w-full object-cover object-center"
+                                            src="{{ Storage::url($category->image) }}" alt="">
+                                    </div>
+                                </div> --}}
+                                <x-navigation-subcategories :category="$category" />
+                            </div>
+                        </li>
+                    @endforeach
                 </ul>
                 <div class="col-span-3 bg-gray-100">
- 
+                    <x-navigation-subcategories :category="$categories->first()" />
                 </div>
             </div>
         </div>
     </nav>
- 
+
 </header>
