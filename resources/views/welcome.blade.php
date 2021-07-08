1d8eeeab79603bdd29d216b271b394cf5537c866
mike trujillo <38016591+mike121986@users.noreply.github.com>
2021-07-08 10:58:45 -0500

    se agrega un componente para recibir los datos de categoria

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,10 @@
 <x-app-layout>
+    <div class="container py-8">
+        <section>
+            <h1 class="text-lg uppercase font-semibold text-gray-700">
+                {{$categories->first()->name}}
+            </h1>
+            @livewire('category-products', ['category' => $categories->first()])
+        </section>
+    </div>
 </x-app-layout>
