23cf251d9221b4c55f662917a4dd73fc4394174e
mike trujillo <38016591+mike121986@users.noreply.github.com>
2021-07-09 17:57:34 -0500

    agregamos una nueva ruta para la vista de productos

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,8 @@ use App\Http\Controllers\CategoryController;
 Route::get('/', WelcomeController::class);
 
 Route::get('categories/{category}',[CategoryController::class, 'show'])->name('categories.show');
+
+Route::get('products/{products}',[ProductController::class, 'show'])->name('products.show');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
